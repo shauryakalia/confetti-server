1d3257e6289e4dc3332bbe23ca21ffb3987e1218
Shaurya.kalia <shaurya.kalia@wingify.com>
2021-11-12 13:45:39 +0530

    init setup

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f9cc818afe153994ecb0ed00fd93458c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '459f0d5b-a7f2-4a46-95ef-05f48832579c'
+};
